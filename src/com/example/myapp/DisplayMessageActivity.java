2be9632548ba5df11f272bff839ac3b954aba3a3
Zach Ewert <zachanima@gmail.com>
2012-06-25 02:19:40 +0200

    Add display message activity class

---
+++ src/com/example/myapp/DisplayMessageActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
