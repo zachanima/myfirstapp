05db36ac19d37f50ea41c6f4b4e2ef7fd7233d1f
Zach Ewert <zachanima@gmail.com>
2012-06-25 18:05:04 +0200

    Add exit button

--- gen/com/example/myapp/R.java
+++ gen/com/example/myapp/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button_exit=0x7f050002;
         public static final int button_send=0x7f050001;
         public static final int edit_message=0x7f050000;
     }
@@ -22,6 +23,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_exit=0x7f040003;
         public static final int button_send=0x7f040002;
         public static final int edit_message=0x7f040001;
     }
