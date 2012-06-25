3dbc2207b1ad131a64278509d1e10812c45c1f05
Zach Ewert <zachanima@gmail.com>
2012-06-25 02:24:16 +0200

    Add textview to display message activity class

--- src/com/example/myapp/DisplayMessageActivity.java
+++ src/com/example/myapp/DisplayMessageActivity.java
@@ -1,11 +1,22 @@
 package com.example.myapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MyFirstActivity.EXTRA_MESSAGE);
+    
+    TextView textView = new TextView(this);
+    textView.setTextSize(40);
+    textView.setText(message);
+    
+    setContentView(textView);
   }
 }
